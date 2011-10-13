8d4ad19c24129a3b03fc9e32eb33bbf430ff4bee
Jason Gritman <jgritman@chariotsolutions.com>
2011-10-13 17:22:48 -0400

    adding data tables, full calendar, dynatree, and bbq

--- grails-app/conf/JQueryResources.groovy
+++ grails-app/conf/JQueryResources.groovy
@@ -4,6 +4,12 @@ modules = {
 		resource url:'js/application.js'					
 	}
 
+	jqueryui {
+		dependsOn 'jquery'
+		resource url: 'js/jquery-ui-1.8.16.custom.min.js'
+		resource url: 'css/south-street/jquery-ui-1.8.16.custom.css'
+	}
+
 	validation {
 		dependsOn 'jquery'
 		resource url: 'js/jquery.validate.js'
@@ -11,4 +17,29 @@ modules = {
 		resource url: 'js/customValidations.js'
 		resource url: 'css/validation.css'
 	}
+
+	datatables {
+		dependsOn 'jqueryui'
+		resource url: 'js/jquery.dataTables.js'
+		resource url: 'css/demo_table_jui.css'
+	}
+
+	fullcalendar {
+		dependsOn 'jqueryui'
+		resource url: 'js/fullcalendar.js'
+		resource url: 'css/fullcalendar.css'
+		resource url: 'css/fullcalendar.print.css', attrs: [media: 'print']
+	}
+
+	dynatree {
+		dependsOn 'jquery'
+		resource url: 'js/jquery.dynatree.js'
+		resource url: 'css/dynatree/skin/ui.dynatree.css'
+	}
+
+	bbq {
+		dependsOn 'jquery'
+		resource url: 'js/jquery.ba-bbq.min.js'
+	}
+
 }
