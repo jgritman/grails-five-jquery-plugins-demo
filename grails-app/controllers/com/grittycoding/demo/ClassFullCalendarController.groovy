d55085ecea9edd13e6172e6b47e01095f3ce4038
Jason Gritman <jasongritman@gmail.com>
2011-10-16 17:39:24 -0400

    fix JSON rendering

--- grails-app/controllers/com/grittycoding/demo/ClassFullCalendarController.groovy
+++ grails-app/controllers/com/grittycoding/demo/ClassFullCalendarController.groovy
@@ -1,25 +1,20 @@
 package com.grittycoding.demo
 
+import grails.converters.*;
+
 class ClassFullCalendarController {
 
     def index() { 
-
     }
 
     def events() {
-		def results = Class.list();
-
-    	render(contentType: "text/json") {
-		    	events = array {
-		            for (e in results) {
-	    				def endDate = (e.startDate + e.course.days).format("yyyy-MM-dd'T'HH:mm:ss")
-	    				def startDate = e.startDate.format("yyyy-MM-dd'T'HH:mm:ss")	 
-		                event start : startDate,
+	    render Class.list().collect {
+	    				def startDate = it.startDate.format("yyyy-MM-dd'T'HH:mm:ss")
+	    				def endDate = (it.startDate + it.course.days - 1).format("yyyy-MM-dd'T'HH:mm:ss")
+		                [ start : startDate,
 		                	end: endDate,
 		                	title: it.course.name,
-							allDay:true			                	
-		            }
-		        }
-    	}
+							allDay:true]
+		} as JSON
     }
 }
